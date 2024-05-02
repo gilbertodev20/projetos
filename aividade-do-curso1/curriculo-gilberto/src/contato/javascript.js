a1f9488a3c8fd054fda97a4f60534894b6cceca8
GilbertoDev20 <giilneto93@gmail.com>
2024-05-02 20:11:00 -0300

    mudanças javascript

--- aividade-do-curso1/curriculo-gilberto/src/contato/javascript.js
+++ aividade-do-curso1/curriculo-gilberto/src/contato/javascript.js
@@ -1,2 +1,43 @@
-const form = document.getElementByld("formulario")
-cosnt 
+const form   = document.getElementById('form')
+const campos = document.querySelectorAll('.required')
+const spans  = document.querySelectorAll('.spans-required')
+const emailRegex =  /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+
+
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636'
+    spans[index].style.display = 'block'
+}
+  function removeError(index){
+    campos[index].style.border = ''
+    spans[index].style.display = 'none'
+  }
+
+
+
+function nameValidate() 
+{
+    if(campos[0].value.length < 3)
+    {
+        setError(0)
+    }
+    else
+    {
+       removeError(0)
+    }
+
+}
+function emailValidate()
+{
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1)
+    }
+    else 
+    { 
+        removeError(1)
+    }
+        
+
+    
+}
